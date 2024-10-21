77c2d38755a87a2787c2ec870cc3c0aa63f68fd8
Erik Riquelme <erikone2311@gmail.com>
2024-10-21 22:45:40 +0200

    Creat a test for strictEquals function

---
+++ src/__tests__/strictEquals.test.ts
@@ -0,0 +1,80 @@
+import strictEquals from "..";
+
+describe("Given the strictEquals function", () => {
+  describe("When it recives a 1 and a 1", () => {
+    test("Then it should return true", () => {
+      const valueA = 1;
+      const valueB = 1;
+
+      const isSameValue = strictEquals(valueA, valueB);
+
+      expect(isSameValue).toBeTruthy();
+    });
+  });
+
+  describe("When it recives a value NaN and a value NaN", () => {
+    test("Then it should return false", () => {
+      const valueA = NaN;
+      const valueB = NaN;
+
+      const isSameValue = strictEquals(valueA, valueB);
+
+      expect(isSameValue).toBeFalsy();
+    });
+  });
+
+  describe("When it recives a 0 and -0 or -0 and 0", () => {
+    test("Then it should return true", () => {
+      const valueA = 0;
+      const valueB = -0;
+
+      const isSameValue = strictEquals(valueA, valueB);
+
+      expect(isSameValue).toBeTruthy();
+    });
+  });
+
+  describe("When it recives a 1 and '1'", () => {
+    test("Then it should return false", () => {
+      const valueA = 1;
+      const valueB = "1";
+
+      const isSameValue = strictEquals(valueA, valueB);
+
+      expect(isSameValue).toBeFalsy();
+    });
+  });
+
+  describe("When it recives a true and false", () => {
+    test("Then it should return false", () => {
+      const valueA = true;
+      const valueB = false;
+
+      const isSameValue = strictEquals(valueA, valueB);
+
+      expect(isSameValue).toBeFalsy();
+    });
+  });
+
+  describe("When it recives a false and false", () => {
+    test("Then it should return true", () => {
+      const valueA = false;
+      const valueB = false;
+
+      const isSameValue = strictEquals(valueA, valueB);
+
+      expect(isSameValue).toBeTruthy();
+    });
+  });
+
+  describe("When it recives a 'Water' and 'oil'", () => {
+    test("Then it should return true", () => {
+      const valueA = "Water";
+      const valueB = "oil";
+
+      const isSameValue = strictEquals(valueA, valueB);
+
+      expect(isSameValue).toBeFalsy();
+    });
+  });
+});
